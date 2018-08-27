504fd97921ac75f3b0f9da48cc04a3d8f672baa4
Lucas Batista <lfebatis@gmail.com>
2018-08-26 21:57:57 -0300

    adding if comparison to windows and linux

--- business/camera.business.js
+++ business/camera.business.js
@@ -1,12 +1,24 @@
 const config = require('../config');
 const execa = require('execa');
+const os = require('os');
 
 class CameraBusiness {
   
   constructor() { }
 
   async snap(pictureName) {
-    return await execa('bin/ffmpeg.exe -f dshow -i video="Integrated Camera" -vframes 1 ' + ('photos/' + pictureName));
+
+	if(os.type() == "Windows_NT") {
+		//Windows
+		//List devices typing in cmd ffmpeg -list_devices true -f dshow -i dummy
+		return await execa('bin/ffmpeg.exe -f dshow -i video="Integrated Webcam" -vframes 1 ' + ('photos/' + pictureName));
+	} else{
+		//Linux
+		//List devices typing in terminal v4l2-ctl --list-devices
+	}
+  
+	//Original
+    //return await execa('bin/ffmpeg.exe -f dshow -i video="Integrated Camera" -vframes 1 ' + ('photos/' + pictureName));
   }
 }
 
